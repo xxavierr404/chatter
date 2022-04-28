eceb9353a3ddfc12b0fc4f7650d11bdd37909d08
Victor Vasiliev <vasilievvict1@yandex.ru>
2022-04-28 03:56:52 +0300

    Purge template to code logic for Firebase from scratch

--- app/src/main/java/com/teamzero/chatter/ui/fragments/auth/data/LoginDataSource.java
+++ app/src/main/java/com/teamzero/chatter/ui/fragments/auth/data/LoginDataSource.java
@@ -1,6 +1,20 @@
 package com.teamzero.chatter.ui.fragments.auth.data;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.teamzero.chatter.MainActivity;
 import com.teamzero.chatter.ui.fragments.auth.data.model.LoggedInUser;
+import com.teamzero.chatter.ui.fragments.auth.login.LoginFragment;
 
 import java.io.IOException;
 
@@ -9,8 +23,26 @@ import java.io.IOException;
  */
 public class LoginDataSource {
 
-    public Result<LoggedInUser> login(String username, String password) {
+    private FirebaseAuth mAuth;
 
+    public Result<LoggedInUser> login(String username, String password) {
+        if(mAuth == null) mAuth = FirebaseAuth.getInstance();
+        return mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(null);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
         try {
             // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
